018cdcbd364e32bad56b56155fa7c2a1c8959606
Pradeep Khatri <pradeepkhatri44@live.com>
2025-05-05 22:49:26 +0530

    commit Project links
    commit Project links

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,3 +1,4 @@
+import { ExternalLink, Github } from "lucide-react";
 import { href } from "react-router-dom";
 
 const projects = [
@@ -60,11 +61,40 @@ export const ProjectsSection = () => {
                 />
               </div>
 
-              <div className="p-6">
+              <div className="p-2">
                 <div className="flex flex-wrap gap-2 mb-4">
-                  {project.tags.map((tag) => {
-                    <span>{tag}</span>;
-                  })}
+                  {project.tags.map((tag, index) => (
+                    <span
+                      className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground"
+                      key={index}
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+              <p className="text-muted-foreground text-sm mb-4">
+                {project.description}
+              </p>
+              <div className="flex justify-between items-center">
+                <div className="flex space-x-3">
+                  <a
+                    href={project.demoUrl}
+                    target="_blank"
+                    className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                  >
+                    {" "}
+                    <ExternalLink size={20} />
+                  </a>
+                  <a
+                    className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    target="_blank"
+                    href={project.githubUrl}
+                  >
+                    <Github size={20} />
+                    2:10
+                  </a>
                 </div>
               </div>
             </div>
