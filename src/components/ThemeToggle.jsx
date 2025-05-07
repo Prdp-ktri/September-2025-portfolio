33ee0d3b46f6e3f6f88d765bf6c95844c4f35407
Pradeep Khatri <pradeepkhatri44@live.com>
2025-05-07 10:56:46 +0530

    commit all working
    commit all working

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -37,9 +37,9 @@ export const ThemeToggle = () => {
       )}
     >
       {isDarkMode ? (
-        <Sun className="h-6 w-6 text-yellow-300" />
+        <Sun className="h-6 w-6 text-yellow-300 mt-20" />
       ) : (
-        <Moon className="h-6 w-6 text-blue-900" />
+        <Moon className="h-6 w-6 text-blue-900 mt-20" />
       )}
     </button>
   );
