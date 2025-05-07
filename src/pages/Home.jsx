2dffee7cdb36b7fc4c5d89521a52e7451c73f827
Pradeep Khatri <pradeepkhatri44@live.com>
2025-05-07 22:47:02 +0530

    commiting Star
    commiting Star

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,14 @@ import { Navbar } from "../components/Navbar";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ThemeToggle } from "../components/ThemeToggle";
-import { StarBackground } from "@/components/STarBackground";
+import { StarBackground } from "@/components/StarBackground";
 import { Footer } from "../components/Footer";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
-      <ThemeToggle /> 
+      <ThemeToggle />
 
       {/* Background Effects */}
       <StarBackground />
