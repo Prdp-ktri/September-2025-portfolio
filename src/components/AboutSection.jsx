b8d9e67486161ada3fb5963575b9241d00ec86e0
Pradeep Khatri <pradeepkhatri44@live.com>
2025-05-08 17:18:15 +0530

    Update AboutSection.jsx adding drive link for CV
    Update AboutSection.jsx adding drive link for CV

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="PRADEEPKHATRI-RESUME_Amd.pdf"
+                href="https://drive.google.com/file/d/155QBIOZetQIXD_dRkPcU1xR7JU0-CdfZ/view?usp=drive_link"
                 className="px-6 py-2 rounded-full border-primary text-primary
               hover:bg-primary/10 transition-colors duration-300 card-hover"
               >
