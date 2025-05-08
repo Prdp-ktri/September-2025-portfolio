2895c1cd1553900f85fdb6f0931f41d39b0a661c
Pradeep Khatri <pradeepkhatri44@live.com>
2025-05-08 17:22:57 +0530

    Update AboutSection.jsx adding target="_blank"
    Update AboutSection.jsx adding target="_blank"

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -37,6 +37,7 @@ export const AboutSection = () => {
                 href="https://drive.google.com/file/d/155QBIOZetQIXD_dRkPcU1xR7JU0-CdfZ/view?usp=drive_link"
                 className="px-6 py-2 rounded-full border-primary text-primary
               hover:bg-primary/10 transition-colors duration-300 card-hover"
+                target="_blank"
               >
                 {" "}
                 Download CV
