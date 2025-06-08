b02f0dfc748bd4f1a585fafa07e7b8691fc143e8
Pradeep Khatri <pradeepkhatri44@live.com>
2025-06-08 15:53:31 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="src/components/PRADEEPKHATRI-RESUME_Amd.pdf"
+                href="https://drive.google.com/file/d/1BR6F_hpqIw93G24mN2y8lwHCGo_EUWgE/view?usp=drive_link"
                 className="px-6 py-2 rounded-full border-primary text-primary
               hover:bg-primary/10 transition-colors duration-300 card-hover"
                 target="_blank"
