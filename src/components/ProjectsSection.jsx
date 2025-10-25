cd76f96df3d074154ac754221f2ff5b39c83fd7d
Pradeep Khatri <pradeepkhatri44@live.com>
2025-10-25 15:26:02 +0530

    commit addition of e-commerce project
    commit addition of e-commerce project

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -5,11 +5,12 @@ const projects = [
   {
     id: 1,
     title: "Finesse Furnishing E-COMMERCE",
-    description: "",
+    description:
+      "This is a furniture-marketplace e-commerce project for buying and selling beds and sofas, hosted on GitHub by Prdp-ktri. It includes frontend and backend code to manage products, users, shopping cart, and transactions.",
     demoUrl: "",
     tags: ["React", "TailwindCSS"],
     githubUrl: "https://github.com/Prdp-ktri/bed-n-sofa-E-commerce-project",
-    image: "",
+    image: "../../projects/ECommerce_buyersportal.jpg",
   },
   {
     id: 2,
