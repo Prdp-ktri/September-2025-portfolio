82cf0d2414c27e57416f8c41bc16172883295caa
Pradeep Khatri <pradeepkhatri44@live.com>
2025-10-25 14:04:40 +0530

    commit new cv
    commit new cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -16,9 +16,9 @@ export const AboutSection = () => {
             </h3>
 
             <p className="text-muted-foreground">
-              With over 1 year of experience learning web development, I specialize in
-              creating responsive, accessible, and performant web applications
-              using modern technologies.
+              With over 1 year of experience learning web development, I
+              specialize in creating responsive, accessible, and performant web
+              applications using modern technologies.
             </p>
 
             <p className="text-muted-foreground">
@@ -34,7 +34,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="https://drive.google.com/file/d/1tA7E4-avFOfHzVHf1WMdb3STijuLOOjX/view?usp=sharing"
+                href="https://drive.google.com/file/d/1lihVxi_N9_XzWaFoJePyD58e1b3fhV6E/view?usp=sharing"
                 className="px-6 py-2 rounded-full border-primary text-primary
               hover:bg-primary/10 transition-colors duration-300 card-hover"
                 target="_blank"
